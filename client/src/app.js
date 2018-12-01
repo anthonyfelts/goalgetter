dcb37d672ec459ba390883ea5b3206f057b6bd07
anthonydevelops <anthonydevelops@gmail.com>
2018-12-01 15:52:50 -0800

    New inputs

--- client/src/app.js
+++ client/src/app.js
@@ -1,7 +1,20 @@
 const form = document.getElementById("app-form");
+const result = document.getElementById("result");
+
+document.onkeypress = function(e) {
+  var keyCode = e.keyCode;
+  if (keyCode == 13) {
+    pressedEnter = true;
+    addGoal();
+    keyCode = 0;
+  }
+};
 
 function addGoal() {
   const goal = document.getElementById("goal").value;
-  console.log(goal);
+  const list = document.createElement("p");
+  const text = document.createTextNode(goal);
+  list.appendChild(text);
+  result.appendChild(list);
   form.reset();
 }
