dc938d4ad50c481db34b9f176001e3d32997183d
Torin Foss <tfoss@csumb.edu>
2018-12-01 18:10:54 -0800

    Update GoalGetter.py

--- GoalGetter.py
+++ GoalGetter.py
@@ -16,17 +16,62 @@ class User(Document):
     descriptions = ListField(StringField(required=True, max_length=1000))
     desiredTimes = ListField(FloatField())
     timeCompleted = ListField(FloatField())
-    status = ListField(IntField)
+    status = ListField(IntField())
+
+
+
+
+
 
 @app.route('/')
 def index():
-    return render_template('client.html')
+    return render_template('register.html')
 
-
-@app.route('/',  methods = ['GET', 'POST'])
-def hello_world():
+@app.route('/register', methods = ['POST'])
+def register():
     if request.method == 'POST':
-        return 'hello'
+        user = request.form['usr']
+        pw1 = request.form['pword']
+        pw2 = request.form['pword2']# no check to see if they are the smae
+        tempuser = User(userName=user, passWord=pw1)
+        tempuser.save()
+        #print("Hello")
+        print(User.objects.count())
+        for ppl in User.objects:
+            print(ppl.userName)
+        return render_template('app.html', user=user)
+
+
+
+
+
+@app.route('/goal',  methods = ['POST'])
+def update(user):
+    print("hello")
+    if request.method == 'POST':
+        tempUser = User.objects(userName=user)
+
+        goal = request.form.get('title')
+        print(goal)
+        # desc = request.form['desc']
+        # time = request.form['time']
+
+        # tempUser.goalNames.append(goal)
+        # tempUser.descriptions.append(desc)
+        # tempUser.desiredTimes.append(time)
+
+        tempUser.save()
+        return("")
+
+        # for ppl in User.objects:
+        #     print(ppl.userName)
+        #     print(ppl.goalNames[0])
+        #     pritn(ppl.descriptions[0])
+        #     print(ppl.desiredTimes[0])
+        #     print()
+        #print(goal)
+
+
 
 #
 # # @app.route('/newuser', methods = ['POST', 'GET'])
